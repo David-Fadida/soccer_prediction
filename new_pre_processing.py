3a1eeb3d1220d2481b6d60070c7aaed89d293b13
David-Fadida <fadidadavid4@gmail.com>
2020-06-18 12:21:03 +0300

    18.06.2020 - date to season

--- new_pre_processing.py
+++ new_pre_processing.py
@@ -1,18 +1,36 @@
 import pandas as pandas
+from datetime import datetime
+
+
+# season calculator
+def get_season(date_string):
+    date = datetime.strptime(date_string[:9], '%Y-%m-%d')
+    if date.month >= 8:
+        return date.year
+    else:
+        date = date - pandas.DateOffset(years=1)
+        return date.year
+
+
+print(get_season('2011-02-22 00:00:00'))
+print(get_season('2014-09-19 00:00:00'))
+print(get_season('2013-09-20 00:00:00'))
 
 try:
     matches_data = pandas.read_csv('datasets/Match.csv')
-    players_data = pandas.read_csv('datasets/Player.csv')
+    teams_data = pandas.read_csv('datasets/Team.csv')
 except FileNotFoundError:
     matches_data = None
-    players_data = None
+    teams_data = None
 
+# Replace date with season's year
+# print(matches_data)
 #
-#   TO DO : new algorithm - old one didn't work
 #
+# print(matches_data)
 
-matches_data.replace()
-print(matches_data)
+# matches_data.loc[:, 'home_player_1':'away_player_11'].replace(id, players_data)
+
 # Save new data csv
-matches_data.to_csv('datasets/Match_Cleaned_New.csv')
+# matches_data.to_csv('datasets/Match_Cleaned_New.csv')
 
